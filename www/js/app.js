df922144b1d6edbded1b229f57904e4634d0cfc9
Sumit Srivastava <ssrivas8@ncsu.edu>
2018-04-07 03:37:44 -0400

    updated code

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,9 @@
 var app = angular.module('root', []);
 
-app.controller('index', ['$scope', '$http', function ($scope, $http) {
+app.controller('index', ['$scope', '$http', '$window', function ($scope, $http, $window) {
 
     $scope.title = 'Bugle Beta App';
+    $scope.loggedInUser = '-';
 
     // Actual User data will be fetched from the session
     $scope.user = {
@@ -23,13 +24,30 @@ app.controller('index', ['$scope', '$http', function ($scope, $http) {
     $scope.events = [
         { 'id': '1', 'name': 'Event 1', 'location': 'Raleigh', 'date': '04.14.18' },
         { 'id': '2', 'name': 'Event 2', 'location': 'Cary', 'date': '03.30.18' },
-        { 'id': '3', 'name': 'Event 3', 'location': 'Morrisville', 'date': '05.11.18' }
+        { 'id': '3', 'name': 'Event 3', 'location': 'Morrisville', 'date': '05.11.18' },
+        { 'id': '4', 'name': 'Event 4', 'location': 'Morrisville', 'date': '05.11.18' },
+        { 'id': '5', 'name': 'Event 5', 'location': 'Morrisville', 'date': '05.11.18' }
+    ];
+    // The actual list will com from the Database via the API
+    $scope.event = { 
+        'id': '1', 'name': 'Event 1', 'location': 'Raleigh', 'date': '04.14.18' 
+    };
+    
+    // The actual list will com from the Database via the API
+    $scope.volunteers = [
+        {'id': '1', 'name': 'Lin'},
+        {'id': '2', 'name': 'Tom'},
+        {'id': '3', 'name': 'Jack'},
+        {'id': '4', 'name': 'John'}
+          
     ];
 
+    // Login function Begin.
     $scope.login = function () {
         console.log('login called');
 
         var loginURL = 'https://bugle-pl-srv.herokuapp.com/login';
+        // var loginURL = 'localhost:9000/login';
         var loginInfo = {
             'email': $scope.email,
             'password': $scope.password
@@ -40,13 +58,66 @@ app.controller('index', ['$scope', '$http', function ($scope, $http) {
             method: 'POST',
             data: loginInfo,
             headers: { 'Content-Type': 'application/json' }
-        }).then(function (response) {
+        }).then(function successLogin(response) {
             console.log('SUCCESS: ' + JSON.stringify(response));
-            $scope.greeting = response.data.status;
-        }, function (response) {
+            var user = JSON.parse(response.data.user);
+            $scope.greeting = response.data.status + '. Hello ' + user.uName;
+            if (user.type === 'vol') {
+                updateScopeUser(user);
+                $window.location.href = '/volunteer.html';
+            } else {
+                updateScopeUser(user);
+                $window.location.href = '/organization.html';
+            }
+           
+        }, function failLogin(response) {
             console.log('ERROR: ' + JSON.stringify(response));
         });
     }
+    // Login function End.
 
-}
-]);
+
+    // Register Vol function Begin
+    $scope.register = function (type) {
+        $scope.dataLoading = true;
+        console.log('register called for ' + type);
+
+        var signupURL = 'https://bugle-pl-srv.herokuapp.com/signup';
+        var signupInfo = {
+            'u_name': $scope.u_name,
+            'email': $scope.email,
+            'dob': $scope.dob,
+            'password': $scope.password,
+            'type': type,
+        };
+
+        $http({
+            url: signupURL,
+            method: 'POST',
+            data: signupInfo,
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            console.log('SUCCESS: ' + JSON.stringify(response));
+            var user = JSON.parse(response.data.user);
+            $scope.greeting = response.data.status + '. Hello ' + user.uName;          
+            if (type === 'vol') {
+                updateScopeUser();
+                $window.location.href = '/volunteer.html';
+            } else {
+                updateScopeUser();
+                $window.location.href = '/organization.html';
+            }
+        }, function (response) {
+            console.log('ERROR: ' + JSON.stringify(response));
+        }).finally(function() {
+            $scope.dataLoading = false;
+        });
+    }
+    //Register Vol function end.
+
+
+    var updateScopeUser = function(user) {
+        console.log('updating scope');
+        $scope.loggedInUser = user;
+    }
+}]);
